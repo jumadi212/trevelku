5c2584b998a49ce24f6236b92f692276fe40c6d7
tiofajri <1711500136@mahasiswa.atmaluhur.ac.id>
2020-11-11 14:16:48 +0700

    TUGAS 1
    maaf terlambat, krena bnyak tugas lain

---
+++ app/src/main/java/ac/id/atmaluhur/jumadi/LoginAct.java
@@ -0,0 +1,15 @@
+package ac.id.atmaluhur.jumadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
---
+++ app/src/main/java/ac/id/atmaluhur/jumadi/RegisterActTwo.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.jumadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_act_two);
+    }
+}
